aadf1fefe474100461f2ae0bb9e1d1316cbfa769
8-Lambda-8 <j.wasle111@gmail.com>
2021-03-18 15:29:16 +0100

    test touch to sense Water

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
 
+
+#define SENSE_PIN 4 //Für Touch: 2, 4, 12, 13, 14, 15, 27
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  Serial.println("EndSetup");
 }
+
+
+int touch_value = 100;
 
 void loop() {
   // put your main code here, to run repeatedly:
+  touch_value = touchRead(SENSE_PIN);
+  Serial.println(touch_value);
 }
